f9f98eb2c8b8740dd6eb015480a4e6afd22e8d2f
SakshiKhele <sakshikhele26@gmail.com>
2026-02-03 22:58:54 +0530

    Create variable.tf
    variable of main.tf

---
+++ user_data/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "value of ami"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "value of instance_type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "vpc_key"
+  type        = string
+
+}
